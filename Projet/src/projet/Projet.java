8690551f2b766ca5088671106f540c677ff927cb
Nicolassaint <71348369+Nicolassaint@users.noreply.github.com>
2022-03-16 14:39:00 +0100

    Création du projet

---
+++ Projet/src/projet/Projet.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projet;
+
+
+/**
+ *
+ * @author Azouz
+ */
+public class Projet {
+
+    
+    
+    public static void main(String[] args) {
+        
+
+    }
+    
+}
